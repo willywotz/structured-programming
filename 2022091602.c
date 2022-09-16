ac30f309b42b7aebaf27a0a65f863e4162f49f33
willywotz <dogajack@gmail.com>
2022-09-16 09:41:56 +0700

    switch case with break

---
+++ 2022091602.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+int main() {
+	int a = 2;
+
+	switch (a) {
+		case 1 : printf("11111\n"); break;
+		case 2 : printf("22222\n"); break;
+		case 3 : printf("33333\n"); break;
+		default: printf("AAAAA\n");
+	}
+
+	return 0;
+}
