30e05e025fc47817cbde62fd2d6adb60a8aca71a
willywotz <dogajack@gmail.com>
2022-09-16 09:41:40 +0700

    switch case

---
+++ 2022091601.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+int main() {
+	int a = 2;
+
+	switch (a) {
+		case 1 : printf("11111\n");
+		case 2 : printf("22222\n");
+		case 3 : printf("33333\n");
+		default: printf("AAAAA\n");
+	}
+
+	return 0;
+}
